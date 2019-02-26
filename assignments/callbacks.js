5044092882ab71523ff506c4c897968fd05e626e
Vanessa McLendon <vanessamclendon@Vanessas-MacBook-Pro-2.local>
2019-02-26 16:16:10 -0500

    commit

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -39,11 +39,21 @@ newItems(items,function(first){
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return items.length;
+
 }
+
+console.log(getLength(items[0].length));
+
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return items.length-1;
 }
+
+console.log(last(items[items.length-1]));
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
@@ -57,6 +67,8 @@ function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 }
+
+
 
 /* STRETCH PROBLEM */
 
